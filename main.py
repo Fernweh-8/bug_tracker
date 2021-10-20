ee95f712c770f4883e6acd07ab137a669af9aad1
fernweh <unforgiving.void@gmail.com>
2021-10-20 21:54:48 +0200

    Innstantiate an IssueList class instance before add_new function call.

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ def main():
     user_name = input("Enter username: ")
     bug_title = input("Enter bug title: ")
     bug_description = input("Enter bug description: ")
-    IssueList.add_new(user = user_name, title = bug_title, description = bug_description)
+    my_issues = IssueList([Issue])
+    my_issues.add_new(user = user_name, title = bug_title, description = bug_description)
     #
     # IssueList.delete_by_id()
-    IssueList.show()
+    my_issues.show()
 
     #
     # write_to_csv()
