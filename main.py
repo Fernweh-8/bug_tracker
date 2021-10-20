d05e6e2d2154f9ba99db3a90c500b3ab446be65b
fernweh <unforgiving.void@gmail.com>
2021-10-20 21:45:26 +0200

    Add to main.py user input to enter parameters for add_new function.

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from issue_list import IssueList
 from csv_writer import write_to_csv
 
 def main():
-
-    IssueList.add_new()
-    IssueList.delete_by_id()
+    user_name = input("Enter username: ")
+    bug_title = input("Enter bug title: ")
+    bug_description = input("Enter bug description: ")
+    IssueList.add_new(user = user_name, title = bug_title, description = bug_description)
+    #
+    # IssueList.delete_by_id()
     IssueList.show()
 
-
-    write_to_csv()
+    #
+    # write_to_csv()
 
 
 if __name__ == '__main__':
