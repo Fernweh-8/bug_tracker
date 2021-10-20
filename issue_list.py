65e4c63a3487240f839f9afc548e98591171f43e
fernweh <unforgiving.void@gmail.com>
2021-10-20 20:43:18 +0200

    Delete from main.py all code related to issue list. Instead create a separate module with a class IssueList and methods creating, deleting and printing the issue list.

---
+++ issue_list.py
@@ -0,0 +1,19 @@
+from typing import List
+from dataclasses import dataclass
+from issue import Issue
+
+@dataclass
+class IssueList:
+    issues: List[Issue]
+
+    def add_new(self, **kwargs):
+        self.issues(Issue(**kwargs))
+
+    def delete_by_id(self, _id):
+        for index, issue in enumerate(self.issues):
+            if issue._id == _id:
+                del self.issues[index]
+
+    def show(self):
+        for issue in self.issues:
+            print(issue)
--- main.py
+++ main.py
@@ -1,40 +1,8 @@
 from status import Status
 from issue import Issue
+from issue_list import IssueList
 
 def main():
-    issue_list = []
-
-    # issue_1 = Issue(user="Anna", title="New bug", description="This is a new bug")
-    # issue_2 = Issue(user="Monika", title="Another bug", description="This is another new bug")
-    #
-    # issue_list.append(issue_1)
-    # issue_list.append(issue_2)
-
-    def create_issue(**kwargs):
-        issue_list.append(Issue(**kwargs))
-
-    # def create_issue(**kwargs):
-    #     return Issue(**kwargs)
-    #
-    # def add_to_list():
-    #     issue_list.append(create_issue)
-
-    create_issue(user="John", title="New issue 1", description="This is the first issue")
-    create_issue(user="Anna", title="New issue 2", description="This is the second issue")
-    create_issue(user="Monika", title="New issue 3", description="This is the third issue")
-
-    # print(issue_list)
-
-    # print(add_to_list())
-
-    def delete_from_list(number):
-        for issue in issue_list:
-            if issue._id == number:
-                issue_list.remove(issue)
-
-    delete_from_list(1)
-
-    print(issue_list)
 
     def write_to_csv(issue):
         with open("C:/Users/Fernweh/PycharmProjects/BugTrackerNew/issues.csv", "w") as csv_file:
