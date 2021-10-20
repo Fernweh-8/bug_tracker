15a4fb888e7a8f8431ee214f8dafdb5407539cdb
fernweh <unforgiving.void@gmail.com>
2021-10-20 21:26:27 +0200

    Add to main.py function calls for Issue object handling (creating, adding to list, deleting from list and printing the list, as well as saving Issue parameters to a csv file.

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from csv_writer import write_to_csv
 
 def main():
 
+    IssueList.add_new()
+    IssueList.delete_by_id()
+    IssueList.show()
+
+
+    write_to_csv()
+
 
 if __name__ == '__main__':
     main()
