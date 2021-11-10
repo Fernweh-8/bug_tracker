79387dc2f816ae35e79786fc05568d663658d2ee
fernweh <unforgiving.void@gmail.com>
2021-11-10 20:36:04 +0100

    Add __init__ file to test folder. Write test for delete_by_id function.

--- tests/test_issue_list.py
+++ tests/test_issue_list.py
@@ -24,13 +24,18 @@ class TestIssueList(unittest.TestCase):
         del self.issue_list
 
     def test_delete_by_id(self):
-        self.assertEqual(True, True)
+        ids = [3, 4, 1]
+        size = len(self.issue_list.issues)
+        for _id in ids:
+            self.issue_list.delete_by_id(_id)
+            new_size = size - 1
+            self.assertEqual(len(self.issue_list.issues, new_size)
 
-    @patch('builtins.print')
-    def test_show(self, mock_print):
-        # TODO: update this string with proper output
-#         expected_output = '''
-# Issue(...)
+#     @patch('builtins.print')
+#     def test_show(self, mock_print):
+#         # TODO: update this string with proper output
+# #         expected_output = '''
+# # Issue(...)
 # Issue(...)
 #         '''
         # we must strip whitespace characters from beginning / end
@@ -41,12 +46,12 @@ class TestIssueList(unittest.TestCase):
         # self.issue_list.show()
 
         # example:
-        print('hello')
-        mock_print.assert_called_with('hello')
-        print('world')
-        mock_print.assert_called_with('world')
-        print('aaa')
-        mock_print.assert_called_with('aaa')
+        # print('hello')
+        # mock_print.assert_called_with('hello')
+        # print('world')
+        # mock_print.assert_called_with('world')
+        # print('aaa')
+        # mock_print.assert_called_with('aaa')
 
         # if you want to check what's inside mock_print use this:
         # sys.stdout.write(str(mock_print.call_args_list) + '\n')
@@ -54,9 +59,9 @@ class TestIssueList(unittest.TestCase):
         # separately to the assert_called_with method
         # example: https://realpython.com/lessons/mocking-print-unit-tests/
 
-    def test_filter_by(self):
-        # list of other assertion methods: https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertEqual
-        self.assertEqual(True, True)
+    # def test_filter_by(self):
+    #     # list of other assertion methods: https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertEqual
+    #     self.assertEqual(True, True)
 
 if __name__ == '__main__':
     unittest.main()
