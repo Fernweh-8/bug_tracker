baa3857c5fed24126136390f8a2c6362d51accb6
fernweh <unforgiving.void@gmail.com>
2021-11-22 18:41:13 +0100

    Add test for filter_by method/

--- tests/test_issue_list.py
+++ tests/test_issue_list.py
@@ -10,14 +10,15 @@ from issue_list import IssueList
 from issue import Issue
 from status import Status
 
+
 class TestIssueList(unittest.TestCase):
     def setUp(self):
         self.issue_list = IssueList([
-            Issue(user = 'Anna', title = 'App does not work'),
-            Issue(user = 'John', title =  'App crashes'),
-            Issue(user = 'Anna', title = 'User account does not exist', status = Status.ON_HOLD),
-            Issue(user = 'Anna', title = 'App still does not work', status = Status.REOPENED),
-            Issue(user = 'John', title = 'Strange behavior', flagged = True),
+            Issue(user='Anna', title='App does not work'),
+            Issue(user='John', title='App crashes'),
+            Issue(user='Anna', title='User account does not exist', status=Status.ON_HOLD),
+            Issue(user='Anna', title='App still does not work', status=Status.REOPENED),
+            Issue(user='John', title='Strange behavior', flagged=True),
         ])
 
     def tearDown(self):
@@ -41,40 +42,42 @@ class TestIssueList(unittest.TestCase):
         self.issue_list.show()
         mock_print.call_args_list = self.issue_list.issues
 
-#         # TODO: update this string with proper output
-# #         expected_output = '''
-# # Issue(...)
-# Issue(...)
-#         '''
-        # we must strip whitespace characters from beginning / end
-        # probably we need to cut only from beginning using lstrip instead
-        # expected_output = expected_output.strip()
+    #         # TODO: update this string with proper output
+    # #         expected_output = '''
+    # # Issue(...)
+    # Issue(...)
+    #         '''
+    # we must strip whitespace characters from beginning / end
+    # probably we need to cut only from beginning using lstrip instead
+    # expected_output = expected_output.strip()
 
-        # we need to call our show method from issue_list (saved in setUp method)
+    # we need to call our show method from issue_list (saved in setUp method)
+
+    # example:
+    # print('hello')
+    # mock_print.assert_called_with('hello')
+    # print('world')
+    # mock_print.assert_called_with('world')
+    # print('aaa')
+    # mock_print.assert_called_with('aaa')
+
+    # if you want to check what's inside mock_print use this:
+    # sys.stdout.write(str(mock_print.call_args_list) + '\n')
+    # we must pass every parameter that is passed to our normal print function
+    # separately to the assert_called_with method
+    # example: https://realpython.com/lessons/mocking-print-unit-tests/
+
+    def test_filter_by(self):
+        self.issue_list.filter_by("Bob", "App not loading")
+        self.assertRaises(AttributeError)
 
 
-        # example:
-        # print('hello')
-        # mock_print.assert_called_with('hello')
-        # print('world')
-        # mock_print.assert_called_with('world')
-        # print('aaa')
-        # mock_print.assert_called_with('aaa')
 
-        # if you want to check what's inside mock_print use this:
-        # sys.stdout.write(str(mock_print.call_args_list) + '\n')
-        # we must pass every parameter that is passed to our normal print function
-        # separately to the assert_called_with method
-        # example: https://realpython.com/lessons/mocking-print-unit-tests/
 
-    # def test_filter_by(self):
-    #     # list of other assertion methods: https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertEqual
-    #     self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
     unittest.main()
-
 
     # to run tests from command line use this (from within this folder):
     # $ python3 -m unittest -v
