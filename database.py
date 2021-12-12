1f737123b301e9b83df88f91cd80c06bb956b9fd
fernweh <unforgiving.void@gmail.com>
2021-12-12 22:37:03 +0100

    Fill issue list with issues to insert into database.

--- database.py
+++ database.py
@@ -10,7 +10,16 @@ with sqlite3.connect(db_filename) as conn:
     if db_is_new:
         print('Creating schema')
         cur = conn.cursor()
-        issues = IssueList().issues
+        new_list = IssueList()
+        new_list.add_new(title="new issue", user="Anna", status=0)
+        new_list.add_new(title="issue", user="AnnaD", status=0)
+        new_list.add_new(title="new", user="AnnaS", status=1)
+        new_list.add_new(title="lala", user="AnnaG", status=1)
+        new_list.add_new(title="new lala", user="AnnaK", status=1)
+        new_list.add_new(title="lala new", user="Anna", status=2)
+        new_list.add_new(title="haha", user="AnnaS", status=3)
+        new_list.add_new(title="what", user="AnnaD", status=3)
+
         cur.execute('''CREATE TABLE issue
                        (id INTEGER PRIMARY KEY NOT NULL, 
                        title TEXT, 
@@ -19,7 +28,7 @@ with sqlite3.connect(db_filename) as conn:
                        flagged INTEGER, 
                        user TEXT, 
                        created TEXT)''')
-        cur.executemany("INSERT INTO issue VALUES (?, ?, ?, ?, ?, ?, ? )", issues)
+        cur.executemany("INSERT INTO issue VALUES (?, ?, ?, ?, ?, ?, ? )", new_list.issues)
         conn.commit()
         cur.execute("SELECT * FROM issue")
         print(cur.fetchall())
